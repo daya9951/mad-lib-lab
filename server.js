046fc191a4ce68b0cebf2a2afc8aa4d5540758c4
daya9951 <dayakarreddy582@gmail.com>
2025-04-15 00:03:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,24 @@ server.use(express.urlencoded({ extended: true }));
 server.use(logger('dev'));
 server.use(express.static(path.join(__dirname, 'public')));
 
-// GET route
+// Explicitly serve index.html
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Random number route
 server.get('/do_a_random', (req, res) => {
   res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`);
 });
 
-// POST route
+// POST route for form
 server.post('/', (req, res) => {
   const { noun, verb, adjective } = req.body;
   const result = `Once upon a time, a ${adjective} ${noun} loved to ${verb} all day long!`;
   res.send(result);
 });
 
-// Start server
+// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
